e143e0ea8254384693269e51d01dadcb82d0456a
이다한 <app1@linkplanner.co.kr>
2018-12-03 11:40:04 +0900

    TEST

---
+++ frontend/components/url.js
@@ -0,0 +1,4 @@
+// const url = 'https://62a4ec44.ngrok.io';
+const url = 'http://localhost:3000/api';
+// const url = 'http://13.125.232.145:3000/api';
+export default url;
---
+++ server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const api = require('./backend/routes');
+
+const steemit = require('./backend/steemAPI');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/public/index.html'); // For React/Redux
+});
+
+app.use('/api', api);
+
+
+app.listen(PORT, error => {
+    error
+    ? console.error("is this happending? ", error)
+    : console.info(`==> 🌎 Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`);
+});
